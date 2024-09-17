104bb1ff4c5fd1c357ea8143096384113ec9414b
lawrhymes <lawrenceime246@gmail.com>
2024-09-17 20:35:04 +0100

    My second commit

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,39 +1,13 @@
 'use client'
+import React from 'react'
 import { useAppDispatch, useAppSelector } from '@/app/redux'
 import { setIsSidebarCollapsed } from '@/state'
-import { Layout, LucideIcon, Menu } from 'lucide-react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
-
-interface SideBarLinkProps {
-  href: string;
-  icon: LucideIcon;
-  label: string;
-  isCollapsed: boolean;
-}
+import { Archive, CircleDollarSign, Clipboard, Layout,  Menu, SlidersHorizontal, User } from 'lucide-react'
+import SidebarLink from './SidebarLink'
 
 
 
 
-const SidebarLink: React.FC<SideBarLinkProps> = (
-  {
-    href,
-    icon: Icon,
-    label,
-    isCollapsed
-  }) => {
-  const pathname = usePathname();
-  const isActive = pathname === href || (pathname === "/" && href === "/dashboard")
-  return (
-    <Link href={href}>
-      <div className={`cursor-pointer flex items-center ${isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? "bg-blue-200 text-white" : ""}`}>
-      <Icon className='w-6 h-6 !text-gray-700'/>
-      <span className={`${isCollapsed ? 'hidden' : 'block'} font-medium text-gray-700`}>{label}</span>
-      </div>
-    </Link>
-  )
-}
 
 const Sidebar = () => {
   const dispatch = useAppDispatch();
@@ -41,11 +15,18 @@ const Sidebar = () => {
 
   const toggleSidebar = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
-
-
-  }
+}
 
   const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72  md:w-64'} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+
+  const sidebarLinks = [
+    { href: '/dashboard', icon: Layout, label: 'Dashboard' },
+    { href: '/inventory', icon: Archive, label: 'Inventory' },
+    { href: '/products', icon: Clipboard, label: 'Products' },
+    { href: '/users', icon: User, label: 'Users' },
+    { href: '/settings', icon: SlidersHorizontal, label: 'Settings' },
+    { href: '/expenses', icon: CircleDollarSign, label: 'Expenses' },
+  ];
 
 
 
@@ -62,7 +43,15 @@ const Sidebar = () => {
 
       {/* LINKS */}
       <div className='flex-grow mt-8'>
-        <SidebarLink href='/dashboard' icon={Layout} label='Dashboard' isCollapsed={isSidebarCollapsed}/>
+      {sidebarLinks.map((link, index) => (
+          <SidebarLink
+            key={index}
+            href={link.href}
+            icon={link.icon}
+            label={link.label}
+            isCollapsed={isSidebarCollapsed}
+          />
+        ))}
       </div>
       {/* FOOTER */}
       <div>
