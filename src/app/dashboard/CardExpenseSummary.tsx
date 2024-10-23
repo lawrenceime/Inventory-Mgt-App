94b307bea4513c42e590694073b47bfca74ade02
lawrhymes <lawrenceime246@gmail.com>
2024-10-23 17:48:19 +0100

    Finalized work on the Card sales and purchase summary

---
+++ src/app/dashboard/CardExpenseSummary.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardExpenseSummary = () => {
+  return (
+    <div className="row-span-3  bg-gray-500">CardExpenseSummary</div>
+  )
+}
+
+export default CardExpenseSummary
